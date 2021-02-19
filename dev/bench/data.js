4a5e60b433064ca982ed6d3c61a44d2e5666bc43
github-action-benchmark <github@users.noreply.github.com>
2021-02-19 16:41:58 +0000

    add sync_mpsc (cargo) benchmark result for 36d7dab504a526edd81d17744aae36f11ab3ee4a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1613752889087,
+  "lastUpdate": 1613752918041,
   "repoUrl": "https://github.com/FSMaxB/tokio",
   "entries": {
     "sync_rwlock": [
@@ -159,6 +159,92 @@ window.BENCHMARK_DATA = {
             "name": "uncontended_concurrent_single",
             "value": 1080,
             "range": "± 51",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "sync_mpsc": [
+      {
+        "commit": {
+          "author": {
+            "email": "te316e89@gmail.com",
+            "name": "Taiki Endo",
+            "username": "taiki-e"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "36d7dab504a526edd81d17744aae36f11ab3ee4a",
+          "message": "chore: remove html_root_url (#3489)\n\nCo-authored-by: Alice Ryhl <alice@ryhl.io>",
+          "timestamp": "2021-02-18T14:11:39-08:00",
+          "tree_id": "8878fcca2ed0b594e0a79a64a96462577bb5f970",
+          "url": "https://github.com/FSMaxB/tokio/commit/36d7dab504a526edd81d17744aae36f11ab3ee4a"
+        },
+        "date": 1613752917089,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "contention_bounded",
+            "value": 6494407,
+            "range": "± 2649226",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_bounded_full",
+            "value": 6348225,
+            "range": "± 1696284",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_unbounded",
+            "value": 5503446,
+            "range": "± 2251055",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_000_medium",
+            "value": 793,
+            "range": "± 155",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_medium",
+            "value": 766,
+            "range": "± 120",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_1_medium",
+            "value": 773,
+            "range": "± 106",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_large",
+            "value": 132855,
+            "range": "± 15978",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_medium",
+            "value": 1724,
+            "range": "± 243",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_bounded",
+            "value": 1030573,
+            "range": "± 150794",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_unbounded",
+            "value": 715635,
+            "range": "± 60368",
             "unit": "ns/iter"
           }
         ]
