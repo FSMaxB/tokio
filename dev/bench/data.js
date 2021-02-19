66a109a7375bb513387206d54f19c690aaae2f90
github-action-benchmark <github@users.noreply.github.com>
2021-02-19 16:41:25 +0000

    add rt_multi_threaded (cargo) benchmark result for 36d7dab504a526edd81d17744aae36f11ab3ee4a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1613752875931,
+  "lastUpdate": 1613752885160,
   "repoUrl": "https://github.com/FSMaxB/tokio",
   "entries": {
     "sync_rwlock": [
@@ -53,6 +53,56 @@ window.BENCHMARK_DATA = {
             "name": "read_uncontended",
             "value": 492,
             "range": "± 24",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "rt_multi_threaded": [
+      {
+        "commit": {
+          "author": {
+            "email": "te316e89@gmail.com",
+            "name": "Taiki Endo",
+            "username": "taiki-e"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "36d7dab504a526edd81d17744aae36f11ab3ee4a",
+          "message": "chore: remove html_root_url (#3489)\n\nCo-authored-by: Alice Ryhl <alice@ryhl.io>",
+          "timestamp": "2021-02-18T14:11:39-08:00",
+          "tree_id": "8878fcca2ed0b594e0a79a64a96462577bb5f970",
+          "url": "https://github.com/FSMaxB/tokio/commit/36d7dab504a526edd81d17744aae36f11ab3ee4a"
+        },
+        "date": 1613752884258,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 181685,
+            "range": "± 37729",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 680659,
+            "range": "± 83609",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 5181171,
+            "range": "± 1091512",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 18606750,
+            "range": "± 2843426",
             "unit": "ns/iter"
           }
         ]
